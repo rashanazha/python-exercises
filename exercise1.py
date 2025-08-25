fc1d7226593dc8f9aac81870bf751ef28dc93d6f
rasha nazha <rasha.nazha@gmail.com>
2025-08-25 19:36:15 +0300

    Initial commit

---
+++ exercise1.py
@@ -0,0 +1,23 @@
+# 
+
+
+# num= int(input("Enter a number:"))
+# if num % 4 == 0:
+#     print(f"{num} is a multiple of 4.")
+# elif num % 2 == 0 :
+#     print("its an even number")
+# else :
+#     print("its an odd number")
+    
+    
+# num = int(input("\nEnter a number to check: "))
+# check = int(input("Enter a number to divide by: "))
+# if num % check == 0 :
+#     print(f"{check} divides evenly into {num}.")
+# else :
+#     print(f"{check} doesn't divide evenly into {num}.")
+x=[5, 10, 15, 20, 25]
+def li() :
+   print([x[0],x[-1]])
+
+li()
